7356af640e5d869cb2b02bec7eab6b1522d1897e
angelGabrielLarios <2021003@utsh.edu.mx>
2024-09-28 21:47:44 -0600

    my commit

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ from pydantic import (
 )
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
-
+load_dotenv()
 class Settings(BaseSettings):
     POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
